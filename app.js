51af73be9b4b34914c4011f54eec75157fe5f22c
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-04-14 13:53:07 +0100

    Updated table / images and getting image thumbnails from Geodesignhub

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ app.use(bodyParser.json())
 var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
 
 app.get('/', function (request, response) {
-    
+
     if (request.query.apitoken && request.query.projectid) {
 
         var api_token = request.query.apitoken;
         var project_id = request.query.projectid;
-        
+
         var cred = "Token " + api_token;
-        
+
         var systems = baseurl + project_id + '/systems/';
         var diagrams = baseurl + project_id + '/diagrams/';
-        
+
         var URLS = [systems, diagrams];
 
         async.map(URLS, function (url, done) {
@@ -44,14 +44,14 @@ app.get('/', function (request, response) {
                 return done(null, JSON.parse(body));
             });
         }, function (err, results) {
-            
+
             if (err) return response.sendStatus(500);
 
             response.render('index', {
                 "status": 1,
-                "data": results, 
-                "api_token":api_token, 
-                "project_id":project_id
+                "data": results,
+                "api_token": api_token,
+                "project_id": project_id
             });
         });
 
@@ -61,21 +61,20 @@ app.get('/', function (request, response) {
     };
 });
 
-app.get('/diagrams', function (request, response) {
-    
-    if (request.query.apitoken && request.query.projectid) {
-
+app.get('/images/:diagram_id(\\d+)/:uuid?/', function (request, response) {
+    let reqUUID = request.params.uuid;
+    let diagram_id = request.params.diagram_id;
+    const is_uuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(reqUUID);
+    diagram_id = parseInt(diagram_id);
+    const is_int = Number.isInteger(diagram_id);
+    if (is_uuid && is_int) {
         var api_token = request.query.apitoken;
         var project_id = request.query.projectid;
-        
         var cred = "Token " + api_token;
-        
-        var systems = baseurl + project_id + '/systems/';
-        var all_diagrams = baseurl + project_id + '/diagrams/all/';
-        
-        
-        var URLS = [ systems, all_diagrams];
-        
+        var all_diagrams = baseurl + project_id + '/diagrams/' + diagram_id + '/thumbnail/' + reqUUID + '/';
+
+        var URLS = [all_diagrams];
+
         async.map(URLS, function (url, done) {
             req({
                 url: url,
@@ -91,12 +90,60 @@ app.get('/diagrams', function (request, response) {
             });
         }, function (err, results) {
             if (err) return response.sendStatus(500);
+            response.writeHead(200, { 'Content-Type': 'image/png' })
+            response.end(results); // Send the file data to the browser.
+
+        });
+
+    } else {
+        response.status(400).send('Not all parameters supplied.');
+
+    };
+});
+
+app.get('/diagrams', function (request, response) {
+
+    if (request.query.apitoken && request.query.projectid) {
+
+        var api_token = request.query.apitoken;
+        var project_id = request.query.projectid;
+
+        var cred = "Token " + api_token;
+
+        var systems = baseurl + project_id + '/systems/';
+        var all_diagrams = baseurl + project_id + '/diagrams/all/details/';
+        var diagram_thumbnails = baseurl + project_id + '/diagrams/all/generate_thumbnails/';
+
+        var URLS = [systems, all_diagrams, diagram_thumbnails];
+
+        async.map(URLS, function (url, done) {
+            req({
+                url: url,
+                headers: {
+                    "Authorization": cred,
+                    "Content-Type": "application/json"
+                }
+            }, function (err, response, body) {
+                if (err || response.statusCode !== 200) {
+                    return done(err || new Error());
+                }
+                return done(null, JSON.parse(body));
+            });
+        }, function (err, results) {
+            if (err) return response.sendStatus(500);
+            let thumbnail_data = results[2];
+
+            var thumbnails_uuid = thumbnail_data['results_url'][0].split('/').slice(-2)[0];
+
+
 
             response.render('diagrams', {
                 "status": 1,
-                "data": results, 
-                "project_id":project_id, 
-                "api_token":api_token
+                "systems": results[0],
+                "thumbnails_uuid": thumbnails_uuid,
+                "all_diagrams": results[1],
+                "project_id": project_id,
+                "api_token": api_token
 
             });
         });
