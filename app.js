f62c1c1821f4b74ea04bbfc0579ac23fe0280a04
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2025-02-17 10:08:01 +0000

    Updated / refactored

--- app.js
+++ app.js
@@ -16,171 +16,124 @@ app.use(bodyParser.json())
 // var baseurl = 'http://local.test:8000/api/v1/projects/';
 var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
 
-app.get('/', function (request, response) {
+app.get('/', async function (request, response) {
+    try {
+        if (!request.query.apitoken || !request.query.projectid) {
+            return response.status(400).send('Not all parameters supplied.');
+        }
 
-    if (request.query.apitoken && request.query.projectid) {
+        const api_token = request.query.apitoken;
+        const project_id = request.query.projectid;
+        const cred = "Token " + api_token;
 
-        var api_token = request.query.apitoken;
-        var project_id = request.query.projectid;
+        const systemsUrl = baseurl + project_id + '/systems/';
+        const diagramsUrl = baseurl + project_id + '/diagrams/';
+        const headers = {
+            "Authorization": cred,
+            "Content-Type": "application/json"
+        };
 
-        var cred = "Token " + api_token;
+        const [systemsResponse, diagramsResponse] = await Promise.all([
+            axios.get(systemsUrl, { headers }),
+            axios.get(diagramsUrl, { headers })
+        ]);
 
-        var systems = baseurl + project_id + '/systems/';
-        var diagrams = baseurl + project_id + '/diagrams/';
-
-        var URLS = [systems, diagrams];
-
-        async.map(URLS, function (url, done) {
-            req({
-                url: url,
-                headers: {
-                    "Authorization": cred,
-                    "Content-Type": "application/json"
-                }
-            }, function (err, response, body) {
-                if (err || response.statusCode !== 200) {
-                    if (response.statusCode == 429) {
-                        return done(err || new Error("Too many requests from your profie"));
-                    }
-                    else {
-                        return done(err || new Error());
-                    }
-                }
-                return done(null, JSON.parse(body));
-            });
-        }, function (err, results) {
-
-            if (err) return response.sendStatus(500);
-
-            response.render('index', {
-                "status": 1,
-                "data": results,
-                "api_token": api_token,
-                "project_id": project_id
-            });
+        response.render('index', {
+            "status": 1,
+            "data": [systemsResponse.data, diagramsResponse.data],
+            "api_token": api_token,
+            "project_id": project_id
         });
-
-    } else {
-        response.status(400).send('Not all parameters supplied.')
-
-    };
+    } catch (error) {
+        if (error.response && error.response.status === 429) {
+            return response.status(429).send("Too many requests from your profile");
+        }
+        response.sendStatus(500);
+    }
 });
 
-app.get('/images/:diagram_id(\\d+)/:uuid?/', function (request, response) {
+app.get('/images/:diagram_id(\\d+)/:uuid?/', async function (request, response) {
+
     let reqUUID = request.params.uuid;
     let diagram_id = request.params.diagram_id;
     const is_uuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(reqUUID);
     diagram_id = parseInt(diagram_id);
     const is_int = Number.isInteger(diagram_id);
-    if (is_uuid && is_int) {
-        var api_token = request.query.apitoken;
-        var project_id = request.query.projectid;
-        var cred = "Token " + api_token;
-        var all_diagrams = baseurl + project_id + '/diagrams/' + diagram_id + '/thumbnail/' + reqUUID + '/';
-        let headers = {
-            "Authorization": cred
-        };
-        var URLS = [all_diagrams];
-        axios
-            .get(all_diagrams, {
-                responseType: 'arraybuffer',
-                headers: headers
-            })
-            .then(res => {
-                let im = Buffer.from(res.data, 'binary').toString('base64');
-                const img = Buffer.from(im, 'base64');
-                response.writeHead(200, {
-                    'Content-Type': 'image/png',
-                    'Content-Length': img.length
-                });
 
-                response.end(img);
-            })
-        //     async.map(URLS, function (url, done) {
-        //         let headers = {
-        //             "Authorization": cred
-        //         };
-        //         let image = getBase64(url, headers);
-        //         return done(null, image);
-        //         // req({
-        //         //     url: url,
-        //         //     headers: {
-        //         //         "Authorization": cred,
-        //         //         "Content-Type": "application/json"
-        //         //     }
-        //         // }, function (err, response, body) {
-        //         //     if (err || response.statusCode !== 200) {
-        //         //         return done(err || new Error());
-        //         //     }
-        //         //     return done(null, JSON.parse(body));
-        //         // });
-        //     }, function (err, results) {
-        //         if (err) return response.sendStatus(500);
-        //         response.setHeader("Content-Type", "image/png");
-        //         response.send(results[0]); // Send the file data to the browser.
+    if (!is_uuid || !is_int) {
+        return response.status(400).send('Invalid parameters supplied.');
+    }
 
-        //     });
+    if (!request.query.apitoken || !request.query.projectid) {
+        return response.status(400).send('Not all parameters supplied.');
+    }
 
-        // } else {
-        //     response.status(400).send('Not all parameters supplied.');
+    const api_token = request.query.apitoken;
+    const project_id = request.query.projectid;
+    const cred = "Token " + api_token;
+    const all_diagrams = `${baseurl}${project_id}/diagrams/${diagram_id}/thumbnail/${reqUUID}/`;
+    const headers = {
+        "Authorization": cred
+    };
 
-        // };
+    try {
+        const res = await axios.get(all_diagrams, {
+            responseType: 'arraybuffer',
+            headers: headers
+        });
+
+        const img = Buffer.from(res.data, 'binary');
+        response.writeHead(200, {
+            'Content-Type': 'image/png',
+            'Content-Length': img.length
+        });
+        response.end(img);
+    } catch (error) {
+        response.sendStatus(500);
     }
 });
 
-app.get('/diagrams', function (request, response) {
+app.get('/diagrams', async function (request, response) {
+    try {
+        if (!request.query.apitoken || !request.query.projectid) {
+            return response.status(400).send('Not all parameters supplied.');
+        }
 
-    if (request.query.apitoken && request.query.projectid) {
+        const api_token = request.query.apitoken;
+        const project_id = request.query.projectid;
+        const cred = "Token " + api_token;
 
-        var api_token = request.query.apitoken;
-        var project_id = request.query.projectid;
+        const systemsUrl = baseurl + project_id + '/systems/';
+        const allDiagramsUrl = baseurl + project_id + '/diagrams/all/details/';
+        const diagramThumbnailsUrl = baseurl + project_id + '/diagrams/all/generate_thumbnails/';
+        const headers = {
+            "Authorization": cred,
+            "Content-Type": "application/json"
+        };
 
-        var cred = "Token " + api_token;
+        const [systemsResponse, allDiagramsResponse, diagramThumbnailsResponse] = await Promise.all([
+            axios.get(systemsUrl, { headers }),
+            axios.get(allDiagramsUrl, { headers }),
+            axios.get(diagramThumbnailsUrl, { headers })
+        ]);
 
-        var systems = baseurl + project_id + '/systems/';
-        var all_diagrams = baseurl + project_id + '/diagrams/all/details/';
-        var diagram_thumbnails = baseurl + project_id + '/diagrams/all/generate_thumbnails/';
+        const thumbnail_data = diagramThumbnailsResponse.data;
+        const thumbnails_uuid = thumbnail_data['results_url'][0].split('/').slice(-2)[0];
 
-        var URLS = [systems, all_diagrams, diagram_thumbnails];
-
-        async.map(URLS, function (url, done) {
-            req({
-                url: url,
-                headers: {
-                    "Authorization": cred,
-                    "Content-Type": "application/json"
-                }
-            }, function (err, response, body) {
-                if (err || response.statusCode !== 200) {
-                    if (response.statusCode == 429) {
-                        return done(err || new Error("Too many requests from your profie"));
-                    }
-                    else {
-                        return done(err || new Error());
-                    }
-                }
-                return done(null, JSON.parse(body));
-            });
-        }, function (err, results) {
-            if (err) return response.sendStatus(500);
-            let thumbnail_data = results[2];
-
-            var thumbnails_uuid = thumbnail_data['results_url'][0].split('/').slice(-2)[0];
-
-            response.render('diagrams', {
-                "status": 1,
-                "systems": results[0],
-                "thumbnails_uuid": thumbnails_uuid,
-                "all_diagrams": results[1],
-                "project_id": project_id,
-                "api_token": api_token
-            });
+        response.render('diagrams', {
+            "status": 1,
+            "systems": systemsResponse.data,
+            "thumbnails_uuid": thumbnails_uuid,
+            "all_diagrams": allDiagramsResponse.data,
+            "project_id": project_id,
+            "api_token": api_token
         });
-
-    } else {
-        response.status(400).send('Not all parameters supplied.')
-
-    };
+    } catch (error) {
+        if (error.response && error.response.status === 429) {
+            return response.status(429).send("Too many requests from your profile");
+        }
+        response.sendStatus(500);
+    }
 });
 
 app.listen(process.env.PORT || 5001);
